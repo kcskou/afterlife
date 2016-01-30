0ab2d68fb43377ac58f5c3c97f39f095a1cbc5dc
xlrn <ncl.yim@gmail.com>
2016-01-30 12:06:02 -0800

    added player and enemy mechanics, collision mechanics

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour {
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Ghost")
+		{
+			print ("DEATH");
+		}   
+	}
+}

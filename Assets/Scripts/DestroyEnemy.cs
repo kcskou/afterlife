37bb8c7a64f8d351663de7c0ba19155c2974b31a
Graham <graham_lee23@msn.com>
2016-01-30 17:38:37 -0800

    Added scoring feature.

--- Assets/Scripts/DestroyEnemy.cs
+++ Assets/Scripts/DestroyEnemy.cs
@@ -1,12 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DestroyEnemy : MonoBehaviour {
+    public Text countText;
+    private int count;
+
+    void Start(){
+        countText.text = "0";
+        count = 0;
+        setCountText();
+    }
+
+    void FixedUpdate() {
+        setCountText();
+
+    }
+
+    void setCountText(){
+        countText.text = "Count: " + count.ToString();
+
+    }
+
 	void OnCollisionEnter2D(Collision2D other)
 	{
 		if (other.gameObject.tag == "Ghost")
 		{
 			Destroy(other.gameObject);
+            count = count + 100;
 		}   
 	}
 }

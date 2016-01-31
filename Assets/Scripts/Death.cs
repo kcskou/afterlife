42b000b08e244ceb459946123af052f989ae81f5
Kelvin Kou <k.kou@alumni.ubc.ca>
2016-01-31 12:43:41 -0800

    integrated game over scene; able to die and restart

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -6,7 +6,7 @@ public class Death : MonoBehaviour {
     public int level;
 	void OnCollisionEnter2D(Collision2D other)
 	{
-		if (other.gameObject.tag == "Ghost" || other.gameObject.tag == "Circle")
+		if (other.gameObject.tag == "Ghost")
 		{
             Application.LoadLevel("Game Over Menu");
             print ("DEATH");

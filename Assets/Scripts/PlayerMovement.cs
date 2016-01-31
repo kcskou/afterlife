fad5c9f7bf0a1e7fa22d5af0f735b3ba8f466290
Noiremann <noirkombatman@gmail.com>
2016-01-30 21:21:26 -0800

    hopefully fixed the conflict

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 5.0f;
+
+	void FixedUpdate()	{
+        if (gameObject != null)
+        {
+            var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            transform.position += move * speed * Time.deltaTime;
+        }
+	}
+
+}

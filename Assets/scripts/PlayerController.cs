df5db6d19d79e714a2b51fc015addb9f595b4423
Kelvin Kou <k.kou@alumni.ubc.ca>
2016-01-30 23:03:41 -0800

    integrated Pause and Score counting feature; added menu scene

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,16 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed = 5.0f, boostMultiplier = 2.0f;
+    private bool gamePaused;
+    public Text pauseText;
 
-	void Start () {
+    void Start () {
         // myBody = this.GetComponent<Rigidbody2D>();  // KKOU
-	}
-	
-	void FixedUpdate () {
+        pauseText.text = "";
+        gamePaused = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                pauseText.text = "Paused";
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pauseText.text = "";
+            }
+        }
+    }
+
+    void FixedUpdate () {
         Vector3 moveVecJoystick = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"),
             CrossPlatformInputManager.GetAxis("Vertical"), 0);
         bool isBoosting = CrossPlatformInputManager.GetButton("Boost");

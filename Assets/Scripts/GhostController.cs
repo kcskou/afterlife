154e022cf610a691d616554fa546a8f9fe3d69e6
xlrn <ncl.yim@gmail.com>
2016-01-30 21:06:48 -0800

    added basic animations

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class GhostController : MonoBehaviour {
 
+	Animator ani;
+
 	//public float speed = 5.0f;
     public GameObject player;
     public int moveSpeed = 4;
@@ -10,6 +12,20 @@ public class GhostController : MonoBehaviour {
     private int MinDist = 1000000;
     private Rigidbody rb;
     private int rotateSpeed = 1;
+
+	//animation states
+	const float STATE_DOWN = 0.1f;
+	const float STATE_UP = 1.1f;
+	const float STATE_LEFT = 2.1f;
+	const float STATE_RIGHT = 3.1f;
+
+	const string DOWN = "0.1";
+	const string UP = "1.1";
+	const string LEFT = "2.1";
+	const string RIGHT = "3.1";
+
+
+	float currentState = STATE_DOWN;
 
     // void FixedUpdate()	{
     //	var move = new Vector3 (Input.GetAxis ("Horizontal2"), Input.GetAxis ("Vertical2"), 0);
@@ -20,6 +36,7 @@ public class GhostController : MonoBehaviour {
     {
         player = GameObject.Find("Player");
         rb = GetComponent<Rigidbody>();
+		ani = this.GetComponent<Animator> ();
         
 
 
@@ -34,8 +51,48 @@ public class GhostController : MonoBehaviour {
 
         // Move ourselves in that direction
         transform.position += direction * moveSpeed * Time.deltaTime;
+		if (direction.x > 0) {
+			changeState (STATE_RIGHT);
+		} else if (direction.x < 0) {
+			changeState (STATE_LEFT);
+		} else if (direction.y < 0) {
+			changeState (STATE_DOWN);
+		} else if (direction.y > 0) {
+			changeState (STATE_UP);
+		}
+
     }
 
+	void changeState(float state) {
+		if (currentState == state) {
+			return;
+		}
+
+		switch (state.ToString()) {
+
+		case DOWN:
+			ani.SetFloat ("Blend", STATE_DOWN);
+			break;
+
+		case UP:
+			ani.SetFloat ("Blend", STATE_UP);
+			break;
+
+		case LEFT:
+			ani.SetFloat ("Blend", STATE_LEFT);
+			break;
+
+		case RIGHT:
+			ani.SetFloat ("Blend", STATE_RIGHT);
+			break;
+		}
+
+		currentState = state;
+	}
+
+	void changeMove(int move) {
+		ani.SetInteger ("idleMove", move);
+	}
 
 
 

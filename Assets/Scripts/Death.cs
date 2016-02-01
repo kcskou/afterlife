040693478efb1f1bf7c26ce44e6e5240d0a40fda
Kelvin Kou <k.kou@alumni.ubc.ca>
2016-01-31 16:23:42 -0800

    added high score feature

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -8,7 +8,14 @@ public class Death : MonoBehaviour {
 	{
 		if (other.gameObject.tag == "Ghost")
 		{
-
+            if (DestroyEnemy.instance)
+            {
+                if (PlayerPrefs.GetInt("highscore", 0) < DestroyEnemy.instance.count)
+                {
+                    PlayerPrefs.SetInt("highscore", DestroyEnemy.instance.count);
+                    PlayerPrefs.Save();
+                }
+            }
             Application.LoadLevel("Game Over Menu");
             print ("DEATH");
            
--- Assets/Scripts/DestroyEnemy.cs
+++ Assets/Scripts/DestroyEnemy.cs
@@ -3,13 +3,26 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class DestroyEnemy : MonoBehaviour {
-    private int count;
+    public int count =0;
     public Text countText;
 
+    public Text highestCountText;
+    public static DestroyEnemy instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
     void Start(){
         countText.text = "0";
         count = 0;
         setCountText();
+        if(highestCountText)
+        {
+            if (PlayerPrefs.GetInt("highscore", 0) == 0) highestCountText.text = "";
+            else
+            highestCountText.text = "Highest Score: " + PlayerPrefs.GetInt("highscore", 0);
+        }
     }
 
     void FixedUpdate() {
